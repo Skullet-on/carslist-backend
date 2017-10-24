63fc996081aec67f44cd38470c7c2f98e4c008a9
Eugene <skullet-on@mail.ru>
2017-10-24 11:22:02 +0300

    Added Sequelize

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 var project = require('./package');
+var models = require('./models');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/cars', function (req, res) {
+	res.send(models.Car.findAll());
 });
 
 app.get('/api/status', function (req, res) {
